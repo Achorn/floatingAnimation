fb51b8195ec410d3ed1666a5dfb1ee45344c97cc
achorn <joshuaachorn@gmail.com>
2023-11-01 12:36:46 -0700

    adding classes for toad interaction

--- script.js
+++ script.js
@@ -1,93 +1,78 @@
-let bottleImg = createCurserImg("./assets/imgs/bottle-trans.png");
-let bookImg = createCurserImg("./assets/imgs/book.png");
-function createCurserImg(asset) {
-  let img = document.createElement("img");
-  img.src = asset;
-  img.classList.add("curser-img");
-  return img;
-}
+// let bottleImg = createCurserImg("./assets/imgs/bottle-trans.png");
+// let bookImg = createCurserImg("./assets/imgs/book.png");
+// function createCurserImg(asset) {
+//   let img = document.createElement("img");
+//   img.src = asset;
+//   img.classList.add("curser-img");
+//   return img;
+// }
 
-class CursorFollower {
-  constructor() {
-    let cursorFollower = document.createElement("div");
-    cursorFollower.classList.add("cursor-img-container");
-    document.body.appendChild(cursorFollower);
-    this._cursorFollower = cursorFollower;
-    addEventListener("mousemove", (event) => {
-      let x = event.pageX;
-      let y = event.pageY;
-      cursorFollower.style.top = y + -20 + "px";
-      cursorFollower.style.left = x + -20 + "px";
-    });
-    this.observers = [];
+// class CursorFollower {
+//   constructor() {
+//     let cursorFollower = document.createElement("div");
+//     cursorFollower.classList.add("cursor-img-container");
+//     document.body.appendChild(cursorFollower);
+//     this._cursorFollower = cursorFollower;
+//     addEventListener("mousemove", (event) => {
+//       let x = event.pageX;
+//       let y = event.pageY;
+//       cursorFollower.style.top = y + -20 + "px";
+//       cursorFollower.style.left = x + -20 + "px";
+//     });
+//     this.observers = [];
 
-    document.getElementById("waterBtn").addEventListener("click", () => {
-      this.selectNewCursorImg(bottleImg, "water");
-    });
-    document.getElementById("readBtn").addEventListener("click", () => {
-      this.selectNewCursorImg(bookImg, "book");
-    });
-    document.getElementById("cancelBtn").addEventListener("click", () => {
-      this.removeSelectedCursorImg();
-    });
-    document.getElementById("toadDivWrapper").addEventListener("click", () => {
-      this.removeSelectedCursorImg();
-    });
-  }
-  addObserver(obs) {
-    this.observers.push(obs);
-  }
-  notify(data) {
-    if (this.observers.length > 0) {
-      this.observers.forEach((observer) => observer.update(data));
-    }
-  }
-  removeSelectedCursorImg() {
-    this._cursorFollower.innerHTML = "";
-    this.notify("");
-  }
+//     // document.getElementById("waterBtn").addEventListener("click", () => {
+//     //   this.selectNewCursorImg(bottleImg, "water");
+//     // });
+//     // document.getElementById("readBtn").addEventListener("click", () => {
+//     //   this.selectNewCursorImg(bookImg, "book");
+//     // });
+//     // document.getElementById("cancelBtn").addEventListener("click", () => {
+//     //   this.removeSelectedCursorImg();
+//     // });
+//     // document.getElementById("toadDivWrapper").addEventListener("click", () => {
+//     //   this.removeSelectedCursorImg();
+//     // });
+//   }
 
-  selectNewCursorImg(newImg, action) {
-    this.removeSelectedCursorImg();
-    this._cursorFollower.appendChild(newImg);
-    this.notify(action);
-  }
-}
+//   removeSelectedCursorImg() {
+//     this._cursorFollower.innerHTML = "";
+//     this.notify("");
+//   }
+
+//   selectNewCursorImg(newImg, action) {
+//     this.removeSelectedCursorImg();
+//     this._cursorFollower.appendChild(newImg);
+//     this.notify(action);
+//   }
+// }
 
 class Toad {
-  constructor(toadState) {
-    this._toadState = toadState;
-    this._toad = document.getElementById("toadImg");
-    this.loadToad();
-    document
-      .getElementById("toadDivWrapper")
-      .addEventListener("mouseenter", () => {
-        this.toadonHoverDisplay();
-      });
-    document
-      .getElementById("toadDivWrapper")
-      .addEventListener("mouseleave", () => {
-        this.toadcurrentStateDisplay();
-      });
+  constructor() {
+    this._toad = document.getElementById("toadDivWrapper");
+    this._animating;
+    this.animation;
+  }
+  resetAnimation() {
+    console.log("resetting animation");
+  }
+  feedAnimation() {
+    this.resetAnimation();
+    console.log("water animatin");
+  }
+  educateAnimation() {
+    this.resetAnimation();
+    console.log("educate animating");
   }
 
-  get toadState() {
-    return this._toadState;
-  }
-  loadToad() {
-    return new Promise((resolve) => {
-      this._toad.addEventListener("load", () => {
-        resolve(this._toad);
-      });
-    });
-  }
-  interactWithToad(action) {
+  interact(action) {
+    console.log(action);
     switch (action) {
       case "water":
         console.log("watering");
         break;
-      case "educate":
-        console.log("watering");
+      case "read":
+        console.log("reading");
         break;
       default:
         console.log("poking");
@@ -107,14 +92,43 @@ class Toad {
 
 class ToadGame {
   constructor() {
-    this._toad = new Toad();
-    let cursorFollower = new CursorFollower();
-    cursorFollower.addObserver(this);
-    this.action;
+    // let cursorFollower = new CursorFollower();
+    this._action = "";
+    this.toad = new Toad();
   }
-  update(action) {
-    this.action = action;
+
+  set action(act) {
+    this._action = act;
+  }
+  interactWithToad() {
+    this.toad.interact(this._action);
+    this._action = "";
   }
 }
 
-new ToadGame();
+let game = new ToadGame();
+
+//Game pieces
+
+//buttons
+let waterBtn = document.getElementById("waterBtn");
+let readBtn = document.getElementById("readBtn");
+let cancelBtn = document.getElementById("cancelBtn");
+let toadBtn = document.getElementById("toadDivWrapper");
+waterBtn.addEventListener("click", () => {
+  game.action = "water";
+  // this.selectNewCursorImg(bottleImg, "water");
+});
+readBtn.addEventListener("click", () => {
+  game.action = "read";
+  // this.selectNewCursorImg(bookImg, "book");
+});
+cancelBtn.addEventListener("click", () => {
+  game.action = "";
+  // this.removeSelectedCursorImg();
+});
+toadBtn.addEventListener("click", () => {
+  console.log("clicked");
+  game.interactWithToad();
+  // this.removeSelectedCursorImg();
+});
